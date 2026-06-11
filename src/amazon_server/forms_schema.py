fa4ec7af0f498531b405ed3ed45248c3b07ec439
Szymbuk <szymekbukowski05@fmail.com>
2026-06-11 13:50:43 +0200

    trying to fix empty github link bug

--- src/amazon_server/forms_schema.py
+++ src/amazon_server/forms_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel,EmailStr, AfterValidator, AnyUrl
+from pydantic import BaseModel, EmailStr, AfterValidator, AnyUrl, field_validator
 from typing import Annotated, TypeAlias,List
 from shared.base_schemas import BaseCV
 
@@ -15,6 +15,14 @@ AcceptedUrl: TypeAlias = Annotated[AnyUrl, AfterValidator(check_specific_hosts)]
 class FormsCV(BaseCV):
     email: EmailStr
     github_link: AcceptedUrl | None = None
+
+    @field_validator('github_link', mode='before')
+    @classmethod
+    def empty_string_to_none(cls, v):
+        # Jeśli przyjdzie pusty tekst (lub same spacje), zamieniamy na True None
+        if isinstance(v, str) and v.strip() == "":
+            return None
+        return v
 
 
 class StatusChangeList(BaseModel):
